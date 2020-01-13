a94112b3a8e33c5fff3e2f9ea76fc508f976a912
Yash Dhamija <yashdhamija4@gmail.com>
2020-01-13 11:04:34 -0500

    POJO data models, closes #2

---
+++ pet-clinic-data/src/main/java/model/Person.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Person {
+    private String firstname;
+    private String lasttname;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLasttname() {
+        return lasttname;
+    }
+
+    public void setLasttname(String lasttname) {
+        this.lasttname = lasttname;
+    }
+}
